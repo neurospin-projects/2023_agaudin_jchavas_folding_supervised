777756544a6248637feb0df7bbe43a3ae817a88c
Joel Chavas <joel.chavas@gmail.com>
2022-05-19 16:04:00 +0200

    Adds create_sets and transform.py

---
+++ contrastive/data/transforms.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  This software and supporting documentation are distributed by
+#      Institut Federatif de Recherche 49
+#      CEA/NeuroSpin, Batiment 145,
+#      91191 Gif-sur-Yvette cedex
+#      France
+#
+# This software is governed by the CeCILL license version 2 under
+# French law and abiding by the rules of distribution of free software.
+# You can  use, modify and/or redistribute the software under the
+# terms of the CeCILL license version 2 as circulated by CEA, CNRS
+# and INRIA at the following URL "http://www.cecill.info".
+#
+# As a counterpart to the access to the source code and  rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty  and the software's author,  the holder of the
+# economic rights,  and the successive licensors  have only  limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using,  modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean  that it is complicated to manipulate,  and  that  also
+# therefore means  that it is reserved for developers  and  experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and,  more generally, to use and operate it in the
+# same conditions as regards security.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license version 2 and that you accept its terms.
+"""
+Transforms used in dataset
+"""
+
+import torchvision.transforms as transforms
+
+from contrastive.augmentations import BinarizeTensor
+from contrastive.augmentations import EndTensor
+from contrastive.augmentations import PaddingTensor
+from contrastive.augmentations import PartialCutOutTensor_Roll
+from contrastive.augmentations import RotateTensor
+from contrastive.augmentations import SimplifyTensor
+from contrastive.augmentations import RemoveRandomBranchTensor
+
+def transform_only_padding(input_size, fill_value):
+    return \
+        transforms.Compose([
+            SimplifyTensor(),
+            PaddingTensor(shape=input_size,
+                          fill_value=fill_value),
+            BinarizeTensor(),
+            EndTensor()
+        ])
+
+
+def transform_foldlabel(sample_foldlabel,
+                        input_size, fill_value,
+                        max_angle, percentage):
+    return \
+        transforms.Compose([
+            SimplifyTensor(),
+            PaddingTensor(shape=input_size, fill_value=fill_value),
+            RemoveRandomBranchTensor(sample_foldlabel=sample_foldlabel,
+                                     percentage=percentage,
+                                     input_size=input_size),
+            RotateTensor(max_angle=max_angle),
+            BinarizeTensor()
+        ])
+
+
+def transform_no_foldlabel(input_size, fill_value, max_angle,
+                           from_skeleton, keep_bottom, patch_size):
+    return \
+        transforms.Compose([
+            SimplifyTensor(),
+            PaddingTensor(shape=input_size, fill_value=fill_value),
+            PartialCutOutTensor_Roll(from_skeleton=from_skeleton,
+                                     keep_bottom=keep_bottom,
+                                     patch_size=patch_size),
+            RotateTensor(max_angle=max_angle),
+            BinarizeTensor()
+        ])
