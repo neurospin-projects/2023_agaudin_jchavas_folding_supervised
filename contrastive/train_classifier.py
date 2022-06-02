2d2c2f3f89483df5e9afb91bdc6341c04ee2dc77
AymericGaudin <aymeric.gaudin@student-cs.fr>
2022-06-02 14:42:25 +0200

    train_classifier (working on ACCpatterns)

--- contrastive/train_classifier.py
+++ contrastive/train_classifier.py
@@ -9,11 +9,13 @@ from torch.utils.data import DataLoader, TensorDataset
 from sklearn.metrics import roc_curve, roc_auc_score, accuracy_score
 from contrastive.models.binary_classifier import BinaryClassifier 
 
+from contrastive.data.utils import read_labels
+
 from contrastive.utils.config import process_config
 from contrastive.utils.logs import set_root_logger_level
 
 
-def load_and_format_embeddings(dir_path, labels_path):
+def load_and_format_embeddings(dir_path, labels_path, config):
     train_embeddings = pd.read_csv(dir_path+'train_embeddings.csv', index_col=0)
     val_embeddings = pd.read_csv(dir_path+'val_embeddings.csv', index_col=0)
     test_embeddings = pd.read_csv(dir_path+'test_embeddings.csv', index_col=0)
@@ -25,20 +27,21 @@ def load_and_format_embeddings(dir_path, labels_path):
     # regroup them in one dataframe (discuss with Joël)
     embeddings = pd.concat([train_embeddings, val_embeddings, test_embeddings],
                            axis=0, ignore_index=False)
-    print("embeddings:", embeddings.iloc[:10,:])
     embeddings.sort_values(by='ID', inplace=True)
-    print(embeddings.iloc[:10,:])
+    print("sorted embeddings:", embeddings.head())
 
     # get the labels (0 = no paracingulate, 1 = paracingulate)
     # /!\ use read_labels
-    labels = pd.read_csv(labels_path)
-    labels.sort_values(by='ID', inplace=True)
-    print(labels.iloc[:10,:])
+    labels = read_labels(labels_path, config.subject_column_name, config.label_names)
+    labels = pd.DataFrame(labels.values, columns=['ID', 'label'])
+    labels.sort_values(by='ID', inplace=True, ignore_index=True)
+    print("sorted labels", labels.head())
     # supposed to contain one column 'ID' and one column 'label' with the actual labels
+    # /!\ multiple labels is not handled
 
     # cast the dataset to the torch format
     X =  torch.from_numpy(embeddings.loc[:, embeddings.columns != 'ID'].values).type(torch.FloatTensor)
-    Y = torch.from_numpy(labels.label.values).type(torch.FloatTensor)
+    Y = torch.from_numpy(labels.label.values.astype('float32')).type(torch.FloatTensor)
 
     return X, Y, n_train, n_val, n_test
 
@@ -63,7 +66,7 @@ def train_classifier(config):
     
     
     # import the embeddings (supposed to be already computed)
-    X,Y,n_train,n_val,_ = load_and_format_embeddings(train_embs_path, train_lab_paths)
+    X,Y,n_train,n_val,_ = load_and_format_embeddings(train_embs_path, train_lab_paths, config)
 
 
     # create and train the model
@@ -90,7 +93,7 @@ def train_classifier(config):
         pass
     else:
         # load embeddings of interest
-        X,Y,n_train,n_val,_ = load_and_format_embeddings(train_embs_path, train_lab_paths)
+        X,Y,n_train,n_val,_ = load_and_format_embeddings(train_embs_path, train_lab_paths, config)
 
     labels_pred = bin_class.forward(X)
     labels_pred = labels_pred.detach().numpy()
