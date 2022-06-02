7fa3982737847f6ed9946ccc437f21313e0a0931
AymericGaudin <aymeric.gaudin@student-cs.fr>
2022-06-02 09:49:29 +0200

    'finished' generate embeddings

--- contrastive/generate_embeddings.py
+++ contrastive/generate_embeddings.py
@@ -1,6 +1,7 @@
 import hydra
 import torch
 import pandas as pd
+import os
 import glob
 
 from contrastive.utils.config import process_config
@@ -8,15 +9,18 @@ from contrastive.models.contrastive_learner_visualization import ContrastiveLear
 from contrastive.data.datamodule import DataModule_Evaluation
 
 
-def embeddings_to_pandas(embeddings, csv_path=None):
+def embeddings_to_pandas(embeddings, csv_path=None, verbose=False):
     # embeddings is the output of the compute_representations function
     # csv_path is the path where to save the csv. Set to None if you want to return the df
-    print(len(embeddings[1]))
     columns_names = ['dim'+str(i+1) for i in range(embeddings[0].shape[1])]
     values = pd.DataFrame(embeddings[0].numpy(), columns=columns_names)
     labels = embeddings[1]
     labels = pd.DataFrame(labels, columns=['ID'])
     df_embeddings = pd.concat([labels, values], axis=1)
+    df_embeddings = df_embeddings.groupby('ID').mean()  # remove one copy each ID
+    if verbose:
+        print("embeddings:", df_embeddings.iloc[:10,:])
+        print("nb of elements:", df_embeddings.shape[0])
     if csv_path:
         df_embeddings.to_csv(csv_path)
     else:
@@ -37,7 +41,7 @@ def compute_embeddings(config):
                                sample_data=data_module)
 
     # fetch and load weights
-    paths = config.model_path+"/logs/lightning_logs/version_0/checkpoints"+r'/*.ckpt'
+    paths = config.model_path+"logs/lightning_logs/version_0/checkpoints"+r'/*.ckpt'
     files = glob.glob(paths)
     print("model_weights:", files[0])
     cpkt_path = files[0]
@@ -45,33 +49,32 @@ def compute_embeddings(config):
 
     model.load_state_dict(checkpoint['state_dict'])
 
+    # create folder where to save the embeddings
+    embeddings_path = config.embeddings_save_path
+    os.makedirs(embeddings_path)
+
     # calculate embeddings for training set and save them somewhere
     print("TRAIN SET")
     train_embeddings = model.compute_representations(data_module.train_dataloader())
-    print("train embeddings:", train_embeddings[0][:10])
-    print(train_embeddings[0].shape)
-    print(train_embeddings[1][:10])
 
     # convert the embeddings to pandas df and save them
     embeddings_to_pandas(train_embeddings,
-                         csv_path=config.embeddings_save_path+"train_embeddings.csv")
+                         csv_path=embeddings_path+"train_embeddings.csv")
 
     # same thing for validation set
     print("VAL SET")
     val_embeddings = model.compute_representations(data_module.val_dataloader())
-    print("validation embeddings:",val_embeddings[0][:10])
 
     embeddings_to_pandas(val_embeddings,
-                         csv_path=config.embeddings_save_path+"val_embeddings.csv")
+                         csv_path=embeddings_path+"val_embeddings.csv")
 
     # /!\ DOESN'T WORK ON TEST => problem in create_datasets
     # same thing for test set
     print("TEST SET")
     test_embeddings = model.compute_representations(data_module.test_dataloader())
-    print("test embeddings:", test_embeddings[0][:10])
 
     embeddings_to_pandas(test_embeddings,
-                         csv_path=config.embeddings_save_path+"test_embeddings.csv")
+                         csv_path=embeddings_path+"test_embeddings.csv")
 
 
 
