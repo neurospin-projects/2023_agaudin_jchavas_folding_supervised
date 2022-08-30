6840ad24ef5c67ff58f140f4effd27062403d6e4
Joël CHAVAS <joel.chavas@gmail.com>
2022-08-30 15:36:10 +0200

    Modifies evaluation pipeline to use all ACC_patterns

--- contrastive/evaluation/embeddings_pipeline.py
+++ contrastive/evaluation/embeddings_pipeline.py
@@ -31,7 +31,7 @@ def preprocess_config(sub_dir, dataset, classifier_name='svm', verbose=False):
     # replace the possibly incorrect config parameters
     cfg.model_path = sub_dir
     cfg.embeddings_save_path = sub_dir + f"/{dataset}_embeddings"
-    cfg.training_embeddings = sub_dir + f"/{dataset}_embeddings/test_embeddings.csv"
+    cfg.training_embeddings = sub_dir + f"/{dataset}_embeddings/full_embeddings.csv"
 
     return cfg
 
@@ -92,7 +92,7 @@ overwrite to True if you still want to compute them.")
 
 
 embeddings_pipeline(
-    "/volatile/jc225751/Runs/43_supervised_contrastive_ACCpatterns/Output/supervised/drop_output_0",
+    "/volatile/jc225751/Runs/43_supervised_contrastive_ACCpatterns/Output/hcp_with_priors",
     dataset='cingulate_ACCpatterns',
     classifier_name='svm',
     overwrite=True)
