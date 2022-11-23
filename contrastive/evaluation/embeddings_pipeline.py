677324c3a8946e9358e5e622315c2543429065be
Joel Chavas <joel.chavas@gmail.com>
2022-11-23 11:55:39 +0100

    Config for supervised NEOFAC_C, HCP-half-2

--- contrastive/evaluation/embeddings_pipeline.py
+++ contrastive/evaluation/embeddings_pipeline.py
@@ -100,7 +100,7 @@ overwrite to True if you still want to compute them.")
 
 
 # embeddings_pipeline("/neurospin/dico/agaudin/Runs/04_pointnet/Output",
-embeddings_pipeline("/neurospin/dico/data/deep_folding/papers/ipmi2023/models/contrastive/trained_on_test_ACCpatterns",
-dataset='test_cingulate_ACCpatterns', verbose=True, classifier_name='svm', overwrite=False)
+embeddings_pipeline("/neurospin/dico/data/deep_folding/papers/ipmi2023/models/contrastive/trained_on_HCP_half_2/supervised/NEOFAC_C",
+dataset='cingulate_ACCpatterns_1', verbose=True, classifier_name='svm', overwrite=False)
 #label_names: ["NEOFAC_A", "NEOFAC_O", "NEOFAC_C", "NEOFAC_N", "NEOFAC_E"]
 #label_names: ["NEOFAC_C"]
