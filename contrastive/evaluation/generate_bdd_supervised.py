815fab875174374fcb68cf749cc980eaaba3aebb
AymericGaudin <aymeric.gaudin@student-cs.fr>
2023-09-06 11:31:09 +0200

    Grid search

--- contrastive/evaluation/generate_bdd_supervised.py
+++ contrastive/evaluation/generate_bdd_supervised.py
@@ -7,8 +7,8 @@ from contrastive.utils.models_database import *
 
 # construct the database
 
-folders = ["/neurospin/dico/agaudin/Runs/09_new_repo/Output/init_test"]
-           #"/neurospin/dico/agaudin/Runs/09_new_repo/Output/grid_searches/step2/S.T.s./STs_both_2/"]
+folders = ["/neurospin/dico/agaudin/Runs/09_new_repo/Output/grid_searches/step2/pericalcarine/both",
+           "/neurospin/dico/agaudin/Runs/09_new_repo/Output/grid_searches/step2/pericalcarine/both2"]
 bdd = []
 visited = []
 
@@ -31,8 +31,8 @@ bdd = post_process_bdd_models(bdd, hard_remove=[], git_branch=False)
 
 
 # save the database
-name = "init_diff"
-save_path = "/neurospin/dico/agaudin/Runs/09_new_repo/Output/init_test"
+name = "pericalcarine"
+save_path = "/neurospin/dico/agaudin/Runs/09_new_repo/Output/grid_searches/step2/pericalcarine/"
 bdd.to_csv(os.path.join(save_path, f"bdd_{name}.csv"), index=True)
 
 
